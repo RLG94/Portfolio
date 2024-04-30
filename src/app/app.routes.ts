fb1ac8c124f726797b956c60eb7d6e3f1c37e179
Roberto <roberto.lago.ubeda@gmail.com>
2024-04-30 19:21:40 +0200

    Commit manual finalizado primer apartado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,14 +9,14 @@ import { ContactComponent } from './components/contact/contact.component';
 
 import { ProyectsComponent } from './components/proyects/proyects.component';
 import { ErrorComponent } from './components/error/error.component';
-import { SocialMediaComponent } from './components/social-media/social-media.component';
+import { TechnologiesComponent } from './components/technologies/technologies.component';
 
 
 export const routes: Routes = [{path:'',component:AboutComponent},
                                {path:'about-me', component:AboutComponent},
                                {path:'projects', component: ProyectsComponent},
                                {path:'contact',component:ContactComponent},
-                               {path:'socialMedia',component:SocialMediaComponent},
+                               {path:'technologies',component:TechnologiesComponent},
                                {path:'**',component:ErrorComponent}
 ];
 
--- src/app/components/technologies/technologies.component.spec.ts
+++ src/app/components/technologies/technologies.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SocialMediaComponent } from './social-media.component';
+import { TechnologiesComponent } from './technologies.component';
 
 describe('SocialMediaComponent', () => {
-  let component: SocialMediaComponent;
-  let fixture: ComponentFixture<SocialMediaComponent>;
+  let component: TechnologiesComponent;
+  let fixture: ComponentFixture<TechnologiesComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SocialMediaComponent]
+      imports: [TechnologiesComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(SocialMediaComponent);
+    fixture = TestBed.createComponent(TechnologiesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
